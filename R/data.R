000734660b39b8d1ea555624e2038baed9e1e217
MrMallIronmaker <725mrm@gmail.com>
2020-05-14 22:21:14 -0700

    added spiral as an exportable object

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#TODO: maybe include some VR motion data too?
+
+spiral <- data.frame(i = seq(0, 4*pi, 0.01)) %>%
+  mutate(
+    circular_part = vector3(x=cos(5*i), y=sin(5*i), z=0),
+    forward_part = vector3(x=0, y=0, z=i),
+    spiral_part = circular_part * i / 4 + forward_part
+  )
