7576c83a97359b513f6f46b17ce5340f04779796
Mark Miller <725mrm@gmail.com>
2020-05-19 07:47:41 -0700

    Remove references to pipe and expand out dplyr

--- tests/testthat/test-vector3.R
+++ tests/testthat/test-vector3.R
@@ -1,5 +1,4 @@
 context("vector3 basics")
-library(vrmvrm)
 library(dplyr)
 
 sqrt_1_3 <- sqrt(1/3)
@@ -11,7 +10,7 @@ foo_pz <- c(0, 0, 1, sqrt_1_3)
 simple_axes_tbl <- data.frame(foo = vector3(x=foo_px, y=foo_py, z=foo_pz))
 
 test_that("Vector3 entries can be extracted", {
-  extracted <- simple_axes_tbl %>%
+  extracted <- dplyr::mutate(simple_axes_tbl %>%
     mutate(
       foo_px = foo$x,
       foo_py = foo$y,
@@ -23,8 +22,7 @@ test_that("Vector3 entries can be extracted", {
 })
 
 test_that("Vector3 entries can be added", {
-  doubled <- simple_axes_tbl %>%
-    mutate(
+  doubled <- dplyr::mutate(simple_axes_tbl,
       bar = foo + foo,
       bar_px = bar$x,
       bar_py = bar$y,
@@ -41,8 +39,7 @@ test_that("Vector3 entries can be added", {
 })
 
 test_that("Vector3 entries can be added to numeric vectors of length 3", {
-  translated_z <- simple_axes_tbl %>%
-    mutate(
+  translated_z <- dplyr::mutate(simple_axes_tbl,
       bar = foo + c(0, 0, 1),
       bar_px = bar$x,
       bar_py = bar$y,
@@ -52,8 +49,7 @@ test_that("Vector3 entries can be added to numeric vectors of length 3", {
   expect_equal(translated_z$bar_py, foo_py)
   expect_equal(translated_z$bar_pz, c(1, 1, 2, 1+sqrt_1_3))
 
-  translated_x <- simple_axes_tbl %>%
-    mutate(
+  translated_x <- dplyr::mutate(simple_axes_tbl,
       bar = c(1, 0, 0) + foo,
       bar_px = bar$x,
       bar_py = bar$y,
@@ -69,15 +65,14 @@ test_that("Vector3 entries can't be added to numeric vectors of various non-3 si
   error_class = "dplyr_error"
   message = "the numeric adding to a vector3 must have length 3"
 
-  expect_error(simple_axes_tbl %>% mutate(bar = foo + c(0, 0, 1, 0)), class = error_class, regexp = message)
-  expect_error(simple_axes_tbl %>% mutate(bar = c(0)+ foo), class = error_class, regexp = message)
-  expect_error(simple_axes_tbl %>% mutate(bar = foo + c(0, 0)), class = error_class, regexp = message)
-  expect_error(simple_axes_tbl %>% mutate(bar = rep(1, 15) + foo), class = error_class, regexp = message)
+  expect_error(dplyr::mutate(simple_axes_tbl, bar = foo + c(0, 0, 1, 0)), class = error_class, regexp = message)
+  expect_error(dplyr::mutate(simple_axes_tbl, bar = c(0)+ foo), class = error_class, regexp = message)
+  expect_error(dplyr::mutate(simple_axes_tbl, bar = foo + c(0, 0)), class = error_class, regexp = message)
+  expect_error(dplyr::mutate(simple_axes_tbl, bar = rep(1, 15) + foo), class = error_class, regexp = message)
 })
 
 test_that("Vector3 can be multiplied both left and right by numeric of length 1", {
-  doubled <- simple_axes_tbl %>%
-    mutate(
+  doubled <- dplyr::mutate(simple_axes_tbl,
       bar = foo * 2,
       baz = 3 * foo
     )
@@ -93,8 +88,7 @@ test_that("Vector3 can be multiplied both left and right by numeric of length 1"
 })
 
 test_that("Vector3 can be multiplied both left and right by numeric vector the same length as the vector3 vector", {
-  doubled <- simple_axes_tbl %>%
-    mutate(
+  doubled <- dplyr::mutate(simple_axes_tbl,
       fac = seq(1, 7, by=2),
       bar = foo * c(1, 2, 3, 4),
       baz = fac * foo
@@ -111,29 +105,25 @@ test_that("Vector3 can be multiplied both left and right by numeric vector the s
 })
 
 test_that("Vector3 distances are correctly computed", {
-  distances <- data.frame(
+  distances <- dplyr::mutate(data.frame(
     foo = vector3(
       x = c(-.5, .5, sqrt(2)/2, 3),
       y = c(-.5, -sqrt(2)/2, .5, 4),
       z = c(sqrt(2)/2, .5, -.5, 0)
-    )
-  ) %>%
-    mutate(
-      distance = distance(foo)
-    )
+    )),
+    distance = distance(foo)
+  )
 
   expect_equal(distances$distance, c(1, 1, 1, 5))
 })
 
 test_that("Vector3 distances are correctly computed from an offset", {
-  distances <- data.frame(
+  distances <- dplyr::mutate(data.frame(
     foo = vector3(
       x = c( 1, -4,  4, 3),
       y = c( 0, -3,  5, 4),
       z = c(-2,  5, -7, 0)
-    )
-  ) %>%
-    mutate(
+    )),
       distance = distance(foo, from=c(0, 2, -3))
     )
 
@@ -141,22 +131,20 @@ test_that("Vector3 distances are correctly computed from an offset", {
 })
 
 test_that("Vector3 can be normalized", {
-  normalized <- data.frame(
+  normalized <- dplyr::mutate(data.frame(
     foo = vector3(
       x = c( 1, 0,  4, 3),
       y = c( 0, 0,  5, 4),
       z = c(-1, 5, -7, 0)
-    )
-  ) %>%
-    mutate(
+    )),
       unit = normalize(foo),
       normalized_distance = normalize(foo) %>% distance,
       normalized_distance_of_5 = normalize(foo, length=5) %>% distance
     )
 
-  expect_equal(normalized$unit$x %>% head(2), c(sqrt(2)/2, 0))
-  expect_equal(normalized$unit$y %>% head(2), c(0, 0))
-  expect_equal(normalized$unit$z %>% head(2), c(-sqrt(2)/2, 1))
+  expect_equal(head(normalized$unit$x, 2), c(sqrt(2)/2, 0))
+  expect_equal(head(normalized$unit$y, 2), c(0, 0))
+  expect_equal(head(normalized$unit$z, 2), c(-sqrt(2)/2, 1))
 
   expect_equal(normalized$normalized_distance, c(1, 1, 1, 1))
   expect_equal(normalized$normalized_distance_of_5, c(5, 5, 5, 5))
