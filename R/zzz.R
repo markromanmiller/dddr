0c3e4f6f6c20fa45012b67aea4af82edb1f64e8d
MrMallIronmaker <725mrm@gmail.com>
2020-05-22 21:59:12 -0700

    stub settings + name change on vignette, fix #34

---
+++ R/zzz.R
@@ -0,0 +1,23 @@
+.onAttach <- function(libname, pkgname) {
+  if (options("dddr.convention") == "default")
+  packageStartupMessage(paste0(
+    "\nUsing default 3D conventions. Please verify these conventions accurately\n",
+    "describe your data. See `vignette(\"conventions\", package = \"dddr\")`\n",
+    "for more information and other convention options.\n",
+    "\n",
+    "Run `options(dddr.convention=\"unity\")` if these conventions are\n",
+    "verified to work, or run `options(dddr.convention=\"none\")` to receive\n",
+    "an error when a convention is queried.\n"
+  ))
+}
+
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.dddr <- list(
+    dddr.convention = "default"
+  )
+  toset <- !(names(op.dddr) %in% names(op))
+  if(any(toset)) options(op.dddr[toset])
+
+  invisible()
+}
